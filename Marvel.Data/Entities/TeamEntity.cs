b55a9b7ec2035630b290ad91fd635dda864d62a2
Dan Ficklin <danficklin@fakemail.com>
2022-02-07 13:29:24 -0500

    Folders

--- Marvel.Data/Entities/TeamEntity.cs
+++ Marvel.Data/Entities/TeamEntity.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,13 @@ namespace Marvel.Data.Entities
 {
     public class TeamEntity
     {
-        
+        [Key]
+        public int TeamId { get; set; }
+        [Required]
+        public string TeamName { get; set; }
+        // public virtual List<CharacterEntity> TeamMembers { get; set; }
+        public string Leader { get; set; }
+        // [ForeignKey(nameof(Movie))]
+        public int? MovieId { get; set; }
     }
 }
